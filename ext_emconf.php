e952ce79bf9fc1bc9c6ef1d1210628f134372f08
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-10-12 12:17:28 +0000

    1.0.0 +++ Update TYPO3 6.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/deal_ebay_077_germany/trunk@86598 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,38 +1,37 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Deal! ebay values for marketplace Germany/77',
-	'description' => 'Import the needed ebay values for the German marketplace! Import is done while enabling this extension. Values are needed by Deal! See: http://typo3-deal.de/typo3conf/ext/deal_ebay_000_us/doc/manual.pdf',
-	'category' => 'be',
-	'shy' => 0,
-	'version' => '0.1.1',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 1,
-	'lockType' => '',
-	'author' => 'Dirk Wildt (Die Netzmacher)',
-	'author_email' => 'http://wildt.at.die-netzmacher.de',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '4.5.0-6.1.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
+$EM_CONF[ $_EXTKEY ] = array(
+  'title' => 'Deal! ebay values for marketplace Germany/77',
+  'description' => 'Import the needed ebay values for the German marketplace! Import is done while enabling this extension. Values are needed by Deal! See: http://typo3-deal.de/typo3conf/ext/deal_ebay_077_germany/doc/manual.pdf',
+  'category' => 'be',
+  'shy' => 0,
+  'version' => '1.0.0',
+  'dependencies' => '',
+  'conflicts' => '',
+  'priority' => '',
+  'loadOrder' => '',
+  'module' => '',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearcacheonload' => 1,
+  'lockType' => '',
+  'author' => 'Dirk Wildt (Die Netzmacher)',
+  'author_email' => 'http://wildt.at.die-netzmacher.de',
+  'author_company' => '',
+  'CGLcompliance' => '',
+  'CGLcompliance_note' => '',
+  'constraints' => array(
+    'depends' => array(
+      'typo3' => '4.5.0-6.2.99',
+    ),
+    'conflicts' => array(
+    ),
+    'suggests' => array(
+    ),
+  ),
+  'suggests' => array(
+  ),
 );
-
 ?>
